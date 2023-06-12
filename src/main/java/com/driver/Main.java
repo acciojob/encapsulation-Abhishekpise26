2270d806adf411e57ee50e2e6639469e16e8446f
Abhishek damodar pise <piseabhishek729@gmail.com>
2023-06-12 15:14:17 +0530

    Commit message describing the changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        // Task 4: Try setting a value to 'name' directly and printing it
+        // This will result in an error because 'name' is a private member variable
+        // and cannot be accessed directly from outside the class
+        // obj.name = "John"; // Uncommenting this line will cause an error
+
+        // Task 6: Set a value to 'name' using setter function and access it using getter function
+        obj.setName("Abhishek");
+        obj.getName();
+
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name; // Task 2: Private member variable 'name'
+
+    // Task 5: Getter function for 'name'
+    public String getName() {
+        return name;
+    }
+
+    // Task 5: Setter function for 'name'
+    public void setName(String newName) {
+        this.name = name;
+    }
 }
